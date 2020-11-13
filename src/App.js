5a5662f2f82f273c30b3ea3e8d59f4f9823b392d
willame mesquita <willamecm@gmail.com>
2020-11-12 21:12:42 -0300

    Estado e Imutabilidade

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import Header from './components/Header'
+
+/**
+ * Componente
+ * Propriedade
+ * Estado & imatabilidade
+ */
+
+function App(){
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    //useState retorna um array com 2 posições
+    //1° variavel com o seu valor inicial 
+    //2° função para atualizamos esse valor 
+
+    function handleAddProject(){
+        //projects.push(`Novo projeto ${Date.now()}`);
+
+        setProjects([ ...projects, `Novo projeto ${Date.now()}` ]);
+
+        console.log(projects);
+    }
+
+    return (
+        <>
+            <Header title="Projects" />
+            <ul>
+                { projects.map(project => <li key={project}>{project}</li>) }
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+        </>
+    );
+}
+
+export default App; 
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,4 @@ module.exports = {
             }
         ]
     },
-    externals: {
-		react: "react"
-	}
 };
